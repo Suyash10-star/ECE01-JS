dd3036a7e70d64b7d5266aebaeb3f2f413d8c6ba
Suyash10-star <suyashrajput235@gmail.com>
2026-02-25 09:50:57 +0530

    learning javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("helllo world");
+ let a=10;
+        console.log(a);
+        console.log(typeof(a));
+        a=false;
+        console.log(a);
+        console.log(typeof(a));
+        a="hello world";
+        console.log(a);
+        console.log(typeof(a));
+        a=null;
+        console.log(a);
+        console.log(typeof(a));
+
+    
